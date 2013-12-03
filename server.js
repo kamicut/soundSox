bfb424dd7d7bff49db0d26989d9fa67cbb68f66c
Marc Farra <marcfarra@gmail.com>
2013-12-03 02:59:25 +0200

    Add mock server model

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 var io = require('socket.io').listen(3000);
 
+var model = new Model();
 io.sockets.on('connection', function(socket) {
-	socket.emit('message', {name:'assets/button2.mp3', params: "reverse"});
 
-	socket.on('message', function(data) {
+	socket.emit('message', "We got your connection!");
+
+	socket.on('newsong', function(data) {
 		console.log("Received " + data);
+		if (typeof(model) == undefined) {
+			socket.emit("timeout", "Model hasn't been created yet.")
+		} else {
+		 	socket.emit{"nextsong", model.getNextSong());	
+		}
 	});	
 
 	socket.on('disconnect', function() {
 		console.log("Client disconnected");
 	});
 });
+
+//Modelling the expressions and the songs
+function Model() {
+	this._model = {} //Internal
+	this._currentSongPlaying = {}
+	this._globalProbabilities = [] //I don't know maybe we need this
+
+	/* Specification of message
+	 * 
+	 * A message is of the form {name: '_.mp3', params: '_', withNoise: true/false}
+	 * This will create a song, apply the params such as gain, fade, echo, etc. 
+	 * and then optionally add noise.
+ 	 */
+	this.getNextSong = function() {
+		//Return the best next song according to the model
+
+		var message = {name:'assets/button2.mp3', params: "reverse", withNoise: true}
+		this._currentSongPlaying = message
+		return message
+	}
+
+	/* Specification of algorithm
+	 *
+	 * To fill
+	 */
+	this._improveModel = function(vector) {
+		//Algorithm to inject vector into the model
+	}
+
+}
+
